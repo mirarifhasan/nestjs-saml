154efabd1160f49441e50cec6aafd6f56a85fd80
Mir Arif Hasan <arif.ishan05@gmail.com>
2023-06-16 12:24:41 +0600

    feat: okta config added

--- src/auth/strategies/saml.strategy.ts
+++ src/auth/strategies/saml.strategy.ts
@@ -7,6 +7,7 @@ import { User } from '../models/user';
 export class SamlStrategy extends PassportStrategy(Strategy, 'saml') {
   constructor() {
     super({
+      // OneLogin configuration
       issuer:
         'https://app.onelogin.com/saml/metadata/8b3f00a8-493e-4398-9bc3-bc7530207cb7',
       callbackUrl: 'localhost:3000/v1/auth/sso/saml/callback',
@@ -14,6 +15,14 @@ export class SamlStrategy extends PassportStrategy(Strategy, 'saml') {
       entryPoint:
         'https://arif-dev.onelogin.com/trust/saml2/http-post/sso/8b3f00a8-493e-4398-9bc3-bc7530207cb7',
       wantAssertionsSigned: true,
+
+      // Okta configuration
+      // issuer: 'http://www.okta.com/exk9znm8tnJItiDkA5d7',
+      // callbackUrl: 'localhost:3000/v1/auth/sso/saml/callback',
+      // cert: 'MIIDqDCCApCgAwIBAgIGAYjC2gJnMA0GCSqGSIb3DQEBCwUAMIGUMQswCQYDVQQGEwJVUzETMBEG A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU MBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi02MDgxNTAyODEcMBoGCSqGSIb3DQEJ ARYNaW5mb0Bva3RhLmNvbTAeFw0yMzA2MTYwNjE2MjdaFw0zMzA2MTYwNjE3MjdaMIGUMQswCQYD VQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsG A1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi02MDgxNTAyODEc MBoGCSqGSIb3DQEJARYNaW5mb0Bva3RhLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC ggEBAMa1YrNqNuV56EJiUX6Q687mRhCmy/hmh3td5iYA7IqIVDVwnx0AfaMq5Wcj3rJ+n3TPhMrK ls0bu9MW151BIZc6A4b3l9k5DCweDjvsFh45lWMzpKRfl6LmuvU1sT3VeM40K36YtLaEubkOQkj3 +5cLOT+R795xFpmO6Busp7+eYRbJ0c2Bk9QimP6I01caq5LjBTyRr+i3ql8dmvoNqIuwjpgEf9T2 TQIzAJkoDsFwf+bVPwUh9jezsr5n46YAgdWAZfF23xnY2PZJjEaIshzPQk9Wbsi6FMsNGEJ9c0x1 8kUHF7wHMy9O8TV9HlWAnIV8oMe2If12HB7/2le6V50CAwEAATANBgkqhkiG9w0BAQsFAAOCAQEA NtWW8o23LvEFujK3n291JpGXoyaXC3F7oHz1rtsy0bslPrnhsD4Q3hWBrl+chP4MltgY8cum3qez QGhU+nnjKrQX5o6Ba1buJXsbiVYvkqrxAFhIJOug0kitbstXrvLk2A+ZVBteA2bbm21anvRSA+3n Gxhu7FSPEkp6ulopOz78Bl8GUl+GEqn8RC0+CQgvivcOtrtwSMG0827iEtBjlg+4SfShMBv1mj0t 3UsnhRhW8omZkKEPuU+y5kPUgyh3cceQHjp/8b4SziZawdEUMK+GZRNnOFuRVC9rJ0QcsxgFWWb3 qXEJ3TK+cXkAI78f6pJ2eTyAcuvnnaqx0IksVg==',
+      // entryPoint:
+      //   'https://dev-60815028.okta.com/app/dev-60815028_hoppscotch_1/exk9znm8tnJItiDkA5d7/sso/saml',
+      // wantAssertionsSigned: true,
     });
   }
 
